d971b7c1a6ab33f11b8057c439c5ce15dee7869d
FiazZafar <fiazzafar430@gmail.com>
2026-03-13 17:32:40 +0500

    setup go_router navigation

---
+++ lib/resources/app_routes.dart
@@ -0,0 +1,16 @@
+import 'package:go_router/go_router.dart';
+import 'package:sss_stars_flutter_assessment/views/auth_page.dart';
+import 'package:sss_stars_flutter_assessment/views/homePage.dart';
+
+class AppRoutes {
+  static final String authPage = '/';
+  static final String homePage = '/homePage';
+
+  static final GoRouter router = GoRouter(
+    initialLocation: authPage,
+    routes: [
+      GoRoute(path: authPage, builder: (context, state) => AuthPage()),
+      GoRoute(path: homePage, builder: (context, state) => Homepage()),
+    ],
+  );
+}
